8761587b59f9e2e69f6e55a802b3cbf875ab0117
Joyee Cheung <joyeec9h3@gmail.com>
2014-11-17 01:18:22 +0800

    localize

--- src/denoise.py
+++ src/denoise.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 
 def prob(E1, E2, t):
-    return 1 if E1 > E2 else np.exp((E1 - E2)/t)
+    return 1 if E1 > E2 else np.exp((E1 - E2) / t)
 
 
 def E_generator(beta, eta, h):
@@ -29,16 +29,33 @@ def E_generator(beta, eta, h):
         xy = np.sum(x * y)
         xsum = np.sum(x)
         return h * xsum - beta * xx - eta * xy
-    return E
+
+    def isValid(i, j, shape):
+        return i >= 0 and j >= 0 and i < shape[0] and j < shape[1]
+
+    def delta_E(E, new, old, x, y):
+        i, j, newval = new
+        _, _, oldval = old
+        result = E
+        result = result - (h * oldval ** 2) + (h * newval ** 2)
+        result = result + (eta * y[i, j] * oldval) - (eta * y[i, j] * newval)
+        adjacent = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        neighbors = [x[i + di, j + dj] for di, dj in adjacent
+                     if isValid(i + di, j + dj, x.shape)]
+        result = result + beta * sum(a * oldval for a in neighbors)
+        result = result - beta * sum(a * newval for a in neighbors)
+        return result
+
+    return E, delta_E
 
 
 def temperature(k, kmax):
-    return 1.0/500 * (1.0/k - 1.0/kmax)
+    return 1.0 / 500 * (1.0 / k - 1.0 / kmax)
 
 
-def simulated_annealing(y, kmax, E, temp_dir):
+def simulated_annealing(y, kmax, E, delta_E, temp_dir):
     x = np.array(y)
-    Ebest = E(x, y)
+    Ebest = Ecur = E(x, y)
     initial_time = time.time()
     energy_record = [[0.0, ], [Ebest, ]]
 
@@ -50,16 +67,20 @@ def simulated_annealing(y, kmax, E, temp_dir):
         for idx in np.ndindex(y.shape):
             if idx[1] % x.shape[1] == 0:
                 print "k = %d, Line %d, Ebest = %.6e" % (k, idx[0], Ebest)
-            E1 = E(x, y)
+            E1 = Ecur
+            old = (idx[0], idx[1], x[idx])
             x[idx] *= -1
-            E2 = E(x, y)
+            new = (idx[0], idx[1], x[idx])
+            E2 = delta_E(Ecur, new, old, x, y)
             p, q = prob(E1, E2, t), random()
             if p > q:
                 accept += 1
+                Ecur = E2
                 if (E2 < Ebest):
                     Ebest = E2
             else:
                 reject += 1
+                Ecur = E1
                 x[idx] *= -1  # flip back
 
         end_time = time.time()
@@ -79,10 +100,11 @@ def simulated_annealing(y, kmax, E, temp_dir):
 
 def denoise_image(image, args):
     data = sign(image.getdata(), {0: -1, 255: 1})
-    E = E_generator(args.beta, args.eta, args.argh)
+    E, delta_E = E_generator(args.beta, args.eta, args.argh)
     temp_dir = os.path.dirname(os.path.realpath(args.output))
     y = np.reshape(data, image.size[::-1])
-    result, energy_record = simulated_annealing(y, args.kmax, E, temp_dir)
+    result, energy_record = simulated_annealing(
+        y, args.kmax, E, delta_E, temp_dir)
     result = sign(result, {-1: 0, 1: 255})
     return Image.fromarray(result).convert('1'), energy_record
 
